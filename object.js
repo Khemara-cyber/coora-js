3306318596288fa48ef63d4fa2d1a12be7443675
Khemara <chankhemarasrun@gmail.com>
2025-06-27 16:00:08 +0700

    complete objec

---
+++ object.js
@@ -0,0 +1,33 @@
+// Object
+
+// const car = {
+//   model: "BMW",
+//   isNew: true,
+//   features: ["self-driving", "auto parking"],
+//   drive() {
+//     console.log("driving");
+//   },
+// };
+// console.log(car.features[0]);
+
+// Object More
+
+const obj = { name: "khemara" };
+
+obj["age"] = 20;
+console.log(obj.name);
+console.log(obj["age"]);
+
+// Object as Array Item
+
+const data = [
+  {
+    id: 1,
+    name: "khemara",
+  },
+  {
+    id: 2,
+    name: "Rickie",
+  },
+];
+console.log(data[0].name);
