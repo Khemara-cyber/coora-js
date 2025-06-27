122701ce99b46071c910a3aec81d60d4de31e1ab
Khemara <chankhemarasrun@gmail.com>
2025-06-27 13:48:15 +0700

    Initial project upload

---
+++ variable.js
@@ -0,0 +1,35 @@
+// Variable (var, let, const)
+// let, const (block-scope)
+// let can reassign
+// const can't
+// var (function-scope)
+
+// var name = "khemara";
+// var name = "Rickie";
+let name = "khemara";
+name = "Rickie";
+console.log("Hello " + name);
+
+// multiple variable
+let names, age, job;
+
+names = "abc";
+age = 19;
+job = "SE";
+
+// String
+
+const txt = "hello";
+const text = `Hello`;
+
+// String concatenation
+
+const n = "khemara";
+const jobs = "teacher";
+
+console.log(`Hello ${name} I know you're a ${jobs} `);
+
+// number (+, -, *, /, %)
+let n1 = 60;
+let n2 = 40;
+console.log(n1 + n2);
